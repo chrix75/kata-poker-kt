2b1e5b1763ba47d7852f415b2c19628e49bc55ef
Christian Sperandio <christian.sperandio@gmail.com>
2021-05-20 22:23:35 +0200

    remove useless methods

--- src/main/kotlin/kata/poker/Hand.kt
+++ src/main/kotlin/kata/poker/Hand.kt
@@ -7,27 +7,4 @@ class Hand {
         get() = ArrayList(_cards)
 
     fun with(card: Card) = apply { this._cards.add(card) }
-
-    fun twoPairs(): List<Card> {
-        val pairs = cardValueOccurrences().filter { entry -> entry.value == 2 }
-            .map { entry -> entry.key }
-
-        if (pairs.size != 2) {
-            return emptyList()
-        }
-
-        return cards.filter { card -> pairs.contains(card.value) }
-    }
-
-
-    private fun cardValueOccurrences(): Map<Int, Int> {
-        val counters = mutableMapOf<Int, Int>()
-        _cards.forEach { card ->
-            val current = counters.getOrDefault(card.value, 0)
-            counters[card.value] = current + 1
-        }
-
-        return counters
-    }
-
 }
